94712fe317e21afd3bd3c4ada4abb51af2128edb
bautirenaudo <bautistarenaudo@gmail.com>
2023-04-09 14:03:41 -0300

    cuarto

--- js.js
+++ js.js
@@ -1,11 +1,17 @@
 (function () {
-    //PARALAX
+
     window.addEventListener("scroll", () => {
         let scrollY = document.documentElement.scrollTop;
 
+        //PARALAX
         document.querySelector(".hero .titulo-centro h1").style.transform = `translateY(${scrollY * -0.2}px)`;
         document.querySelector(".hero .titulo-centro .linea").style.transform = `translateX(${scrollY * 0.2}px)`;
         document.querySelector(".hero .titulo-centro button").style.cssText = `translate:0 ${scrollY * 0.1}px;`;
+        //CAMBIOS A PARTIR DE HERO
+        if ((window.scrollY + 30) > document.querySelector(".fondo").scrollHeight) {
+        } else {
+        }
+
     })
 
 
@@ -16,6 +22,7 @@
         document.querySelector("nav .menu-button").addEventListener("click", () => {
             if (indice == 0) {
                 document.querySelector("nav").style.cssText = "transform: translateX(calc(0% - 12px));";
+                document.querySelector("nav").style.background = "var(--main-color)";
                 document.querySelector("nav .menu-button").style.cssText = "transform: rotate(-180deg);";
                 setTimeout(() => {
                     document.querySelectorAll(".ul1 li span").forEach(e => {
@@ -26,10 +33,11 @@
             }
             else if (indice == 1) {
                 document.querySelector("nav").style.cssText = "transform: translateX(calc(-100% + 12px));";
+                document.querySelector("nav").style.background = "var(--main-color)";
                 document.querySelector("nav .menu-button").style.cssText = "transform: rotate(0deg);"
-                    document.querySelectorAll(".ul1 li span").forEach(e => {
-                        e.style.cssText = "width: 0%;";
-                    })
+                document.querySelectorAll(".ul1 li span").forEach(e => {
+                    e.style.cssText = "width: 0%;";
+                })
                 indice = 0;
             }
         })
@@ -56,6 +64,21 @@
     }
     carrusel()
 
+    const variablesColores = (color) => {
+        switch (color) {
+            case "celeste":
+                return "#5BA8FF";
+            case "naranja":
+                return "#FF965B";
+            case "rojo":
+                return "#FF5B5B";
+            case "verde":
+                return "#5BFFD8";
+            case "rosa":
+                return "#FF5BBE";
+        }
+
+    }
     const asignarPaleta = () => {
         document.querySelectorAll(".paleta .color").forEach((colorElement, i) => {
             colorElement.addEventListener("click", () => {
